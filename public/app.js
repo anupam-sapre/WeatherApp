37fbedb17e8e4092a1d6e82b6bb45e28efd40e43
anupamsapre <anupamsapre@ccs.neu.edu>
2017-12-10 01:24:11 -0500

    Setting up session and services for login

---
+++ public/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Anupam on 12/10/2017.
+ */
+(function(){
+    angular.module("WeatherApp",["ngRoute"]);
+})();
--- server.js
+++ server.js
@@ -1,18 +1,37 @@
-/**
- * Created by Anupam on 12/9/2017.
- */
 var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+var passport = require('passport');
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-//require ("./test/app.js")(app);
 
-var port = process.env.PORT || 3000;
+var connectionString = 'mongodb://127.0.0.1:27017/test';
+
+if(process.env.MLAB_USER_WEATHER) {
+    var username =     process.env.MLAB_USER_WEATHER;
+    var password =     process.env.MLAB_PASSWORD_WEATHER ;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds133876.mlab.com:33876/heroku_6tt8hrgt';
+}
+var mongoose = require('mongoose');
+mongoose.connect(connectionString);
+
+require("./project/app.js")(app);
+
+//var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.PORT || 3000;
+
 
 app.listen(port);
