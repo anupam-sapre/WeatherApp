1fe1abcb1d131dfeabc56b3c1333f669acd7f584
anupamsapre <anupamsapre@ccs.neu.edu>
2017-12-12 03:13:32 -0500

    Changes for forecast

--- public/views/home.controller.client.js
+++ public/views/home.controller.client.js
@@ -10,14 +10,23 @@
         vm.showHistory = showHistory;
         vm.updateFields = updateFields;
         vm.logout = logout
+        vm.generateGraph = generateGraph;
+        vm.showCurrentWeather = showCurrentWeather
+
+        vm.generateGraphFlag = false;
         vm.placeError = false;
         vm.timeError = false;
+
 
         function init() {
             vm.searchHistory=[];
             vm.searchDate = moment().format('LLLL');
             vm.searchInput =""
             vm.toggleFlag =true
+            vm.minTemp=[]
+            vm.maxTemp=[]
+            vm.options={}
+            vm.data={}
         }
         init();
         function searchWeather(place,time){
@@ -35,11 +44,27 @@
                 console.log(place)
                 var lat = place.geometry.location.lat();
                 var lng = place.geometry.location.lng();
+                var unixTime = moment(time).unix()
+                generateGraph(lat,lng,time)
                 UserService
-                    .findWeather(lat, lng, moment(time).unix())
+                    .findWeather(lat, lng,unixTime)
                     .then(function (response) {
                             console.log(response.data)
-                            vm.weatherData = response.data.offset
+
+                            vm.data = [
+                                {
+                                    values: vm.maxTemp.sort(function(a,b) {return (a.x > b.x) ? 1 : ((b.x > a.x) ? -1 : 0);}),      //values - represents the array of {x,y} data points
+                                    key: 'Max Temperature', //key  - the name of the series.
+                                    color: '#ff7f0e'  //color - optional: choose your own line color.
+                                },
+                                {
+                                    values: vm.minTemp.sort(function(a,b) {return (a.x > b.x) ? 1 : ((b.x > a.x) ? -1 : 0);}),
+                                    key: 'Min Temperature',
+                                    color: '#2ca02c'
+                                }
+                            ]
+                            vm.generateGraphFlag=true;
+                            showCurrentWeather(response.data)
                             if(vm.currUser) {
                                 UserService.addHistory(vm.currUser._id, lat, lng, moment(time).format('LLLL'),place.formatted_address)
                                     .then(
@@ -47,6 +72,8 @@
                                             vm.success = "Added successfully";
                                             vm.toggleFlag=true
                                             showHistory();
+
+
                                         }
                                         , function (err) {
                                             vm.error = "Unable to save history";
@@ -110,6 +137,108 @@
                     }
                 )
         }
+
+
+        function showCurrentWeather(data) {
+
+            var icons = new Skycons({"color": "Black"});
+            switch(data.currently.icon){
+                case 'clear-day':
+                    icons.add("icon", Skycons.CLEAR_DAY);
+                    break;
+                case 'clear-night':
+                    icons.add("icon", Skycons.CLEAR_NIGHT);
+                    break;
+                case 'partly-cloudy-day':
+                    icons.add("icon", Skycons.PARTLY_CLOUDY_DAY);
+                    break;
+                case 'partly-cloudy-night':
+                    icons.add("icon", Skycons.PARTLY_CLOUDY_NIGHT);
+                    break;
+                case 'cloudy':
+                    icons.add("icon", Skycons.CLOUDY);
+                    break;
+                case 'rain':
+                    icons.add("icon", Skycons.RAIN);
+                    break;
+                case 'sleet':
+                    icons.add("icon", Skycons.SLEET);
+                    break;
+                case 'snow':
+                    icons.add("icon", Skycons.SNOW);
+                    break;
+                case 'wind':
+                    icons.add("icon", Skycons.WIND);
+                    break;
+                case 'fog':
+                    icons.add("icon", Skycons.FOG);
+                    break;
+
+            }
+            icons.play();
+            vm.currentTemp = data.currently.temperature
+            vm.summary = data.currently.summary
+            vm.currentTime = moment.unix(data.currently.time).format('LLLL')
+
+        }
+
+
+
+
+
+        function generateGraph(lat,lng,time) {
+            vm.minTemp=[]
+            vm.maxTemp=[]
+            for(i=1;i<=10    ;i++){
+                var newTime = moment(time).subtract(i, 'years').unix()
+                UserService
+                    .findWeather(lat, lng,newTime)
+                    .then(function (response) {
+                            vm.maxTemp.push({x:moment.unix(response.data.currently.time).year(),y:response.data.daily.data[0].temperatureHigh})
+                            vm.minTemp.push({x:moment.unix(response.data.currently.time).year(),y:response.data.daily.data[0].temperatureMin})
+                        },
+                        function (err) {
+                            vm.error = "Please try after some time";
+                        });
+
+            }
+        }
+
+
+
+
+        vm.options = {
+            chart: {
+                type: 'lineChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 40,
+                    left: 55
+                },
+                x: function(d){ return d.x; },
+                y: function(d){ return d.y; },
+                showValues: true,
+
+                xAxis: {
+                    axisLabel: 'Year',
+
+                },
+                yAxis: {
+                    axisLabel: 'Temperature',
+
+                },
+            },
+            title: {
+                enable: true,
+                text: 'Temperature for past 10  years on this day'
+            },
+
+        };
+
+
+
 
 
     }
